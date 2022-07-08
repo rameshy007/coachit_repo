5ec8ce3843737bc8fe09c58c7b2297780820d557
rameshy007 <sheelamramesh454@gmail.com>
2022-07-08 17:25:53 +0530

    commit

--- demo2/src/files/FileInputStreamDemo.java
+++ demo2/src/files/FileInputStreamDemo.java
@@ -1,13 +1,11 @@
 package files;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
 public class FileInputStreamDemo {
 	public static void main(String[] args) throws IOException {
-		File file = new File("E:\\local_repos\\coachit_repo\\demo2\\demo.txt");
-		FileInputStream fis = new FileInputStream(file);
+		FileInputStream fis = new FileInputStream("E:\\local_repos\\coachit_repo\\demo2\\demo.txt");
 		int c = fis.read();
 		System.out.println(c);
 		while (c != -1) {
